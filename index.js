593e7e926ba4b52e600857cc0d5b8c14ede60e73
jspg2022 <116121073+jspg2022@users.noreply.github.com>
2023-10-04 19:30:29 +0300

    Step 3.3
    3.3 Have a Favorite icon, on the image card, to save the image for later in favorites

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ const modalContent = modal.querySelector('.modalContent');
 const searchInput = document.querySelector('.searchInput');
 const searchBtn = document.querySelector('.searchBtn');
 const imageContainer = document.querySelector('.imageContainer');
+const favoriteContainer = document.querySelector('.favorites-container');
 const moreImagesBtn = document.querySelector('.more-images-btn');
 const endData = document.querySelector('.endData');
+const favoriteBtn = document.querySelector('.favoriteBtn');
 
-let currentPage;
+const favoritesArr = [];
 const perPage = 20;
+let currentPage;
 let searchInputVal;
-// let calculateTotalPages; // add for more images function-To prevent an unnecessary request
+// let calculateTotalPages; // need to add for more images function-To prevent an unnecessary request?? // by divide yotal with perpage
 
 const handleSearchClick = async () => {
   imageContainer.innerHTML = '';
@@ -23,37 +26,89 @@ const handleSearchClick = async () => {
   if (searchInputVal.length > 0) {
     const data = await getSearchData(searchInputVal, currentPage, perPage);
     if (data?.length > 0) {
-      displayCards(data);
+      displayCards(data, imageContainer);
       moreImagesBtn.classList.remove('hidden');
     }
   } else {
-    console.log(searchInputVal.length);
     imageContainer.innerHTML = 'No search field entered, Search again';
   }
 };
 
-const displayCards = (data) => {
+const displayCards = (data, container) => {
   data.forEach((hit) => {
     const card = createImageCard(hit);
-    imageContainer.appendChild(card);
+    container.appendChild(card);
   });
 };
 
-const createImageCard = (imageData) => {
-  const card = document.createElement('div');
-  card.classList.add('card');
-
+const creatImageElement = (imageData) => {
   const image = document.createElement('img');
   image.classList.add('image');
   image.src = imageData.webformatURL;
   image.alt = imageData.tags;
 
-  card.appendChild(image);
+  return image;
+};
 
-  card.onclick = () => {
+const createImageCard = (imageData) => {
+  const card = document.createElement('div');
+  const divIcon = document.createElement('div');
+  card.classList.add('card');
+
+  const image = creatImageElement(imageData);
+  const favoriteIcon = createFavoriteIcon(imageData);
+
+  card.appendChild(image);
+  card.appendChild(divIcon);
+  divIcon.appendChild(favoriteIcon);
+
+  card.setAttribute('data-id', imageData.id);
+
+  image.onclick = () => {
     displayModal(imageData);
   };
+
+  favoriteIcon.onclick = () => {
+    handleFavoriteIcon(imageData, favoriteIcon);
+  };
   return card;
+};
+
+const createFavoriteIcon = (imageData) => {
+  const index = favoritesArr.findIndex((img) => img.id === imageData.id);
+
+  const favoriteIcon = document.createElement('p');
+  index === -1
+    ? (favoriteIcon.innerHTML = '🤍')
+    : (favoriteIcon.innerHTML = '🖤');
+
+  favoriteIcon.classList.add('favoriteIconI');
+  return favoriteIcon;
+};
+
+const handleFavoriteIcon = (imageData, favoriteIcon) => {
+  console.log(1);
+  const index = favoritesArr.findIndex((img) => img.id === imageData.id);
+  if (index === -1) {
+    favoritesArr.push(imageData);
+  } else {
+    favoritesArr.splice(index, 1);
+  }
+
+  //
+  //
+  // Update heart icons in both containers
+  const imageCards = document.querySelectorAll(`[data-id="${imageData.id}"]`);
+  imageCards.forEach((imageCard) => {
+    const imageFavoriteIcon = imageCard.querySelector('.favoriteIconI');
+    if (imageFavoriteIcon) {
+      imageFavoriteIcon.innerHTML = index === -1 ? '🖤' : '🤍';
+    }
+  });
+
+  // Clear and re-render the favoriteContainer
+  favoriteContainer.innerHTML = '';
+  displayCards(favoritesArr, favoriteContainer);
 };
 
 const displayModal = (imageData) => {
@@ -74,12 +129,12 @@ const handleCloseModal = () => {
 };
 
 const handleMoreImages = async () => {
-  currentPage = currentPage + 20;
+  currentPage++;
 
   try {
     const data = await getSearchData(searchInputVal, currentPage, perPage);
     if (data?.length > 0) {
-      displayCards(data);
+      displayCards(data, imageContainer);
     } else {
       endData.innerHTML = 'No more images found for this search.';
       moreImagesBtn.classList.add('hidden');
@@ -91,7 +146,26 @@ const handleMoreImages = async () => {
   }
 };
 
+const handleFavoritesBtn = () => {
+  const isFavoriteContainerVisible =
+    favoriteContainer.style.display === 'block';
+
+  if (isFavoriteContainerVisible) {
+    favoriteContainer.style.display = 'none'; // Hide the favorites container
+  } else {
+    if (favoritesArr.length > 0) {
+      favoriteContainer.innerHTML = ''; // Clear the previous favorites
+      displayCards(favoritesArr, favoriteContainer);
+    } else {
+      favoriteContainer.innerHTML = 'No favorite photos selected';
+    }
+
+    favoriteContainer.style.display = 'block'; // Show the favorites container
+  }
+};
+
 searchBtn.addEventListener('click', handleSearchClick);
 closeModal.addEventListener('click', handleCloseModal);
 overlay.addEventListener('click', handleCloseModal);
 moreImagesBtn.addEventListener('click', handleMoreImages);
+favoriteBtn.addEventListener('click', handleFavoritesBtn);
